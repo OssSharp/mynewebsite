d435202cc14a238c27602893874bd4fcafafc392
OgLog <saripovolzas04@gmail.com>
2026-01-11 22:06:03 +0300

    force custom domain redirect

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
-from flask import Flask
-
+from flask import Flask, request, redirect
 
 def create_app():
     app = Flask(__name__)
+
+    @app.before_request
+    def force_custom_domain():
+        if request.host == "mynewebsite-3.onrender.com":
+            return redirect(
+                "https://accountsmheducationcom.help" + request.full_path,
+                code=301
+            )
 
     from .routes import routes
     app.register_blueprint(routes)
 
     return app
-
-
-app = create_app()
